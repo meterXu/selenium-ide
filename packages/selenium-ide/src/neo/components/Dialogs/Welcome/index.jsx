09c22a1cea9e028330ebf9cbc1abd1dca2d62a7e
meter <772413635@qq.com>
2020-01-03 14:10:40 +0800

    ....

--- packages/selenium-ide/src/neo/components/Dialogs/Welcome/index.jsx
+++ packages/selenium-ide/src/neo/components/Dialogs/Welcome/index.jsx
@@ -82,46 +82,46 @@ class WelcomeDialogContents extends React.Component {
         renderTitle={() => (
           <div>
             <div className="welcome-dialog__title">
-              Welcome to Selenium IDE!
+              欢迎使用 Selenium IDE!
             </div>
             <div className="welcome-dialog__subtitle">
-              Version {project.version}
+              版本 {project.version}
             </div>
           </div>
         )}
         renderFooter={() => (
           <p>
-            To learn more on Selenium IDE and how to use it visit the{' '}
+            要了解有关Selenium IDE及其使用方法的更多信息，请访问{' '}
             <a
               href="https://www.seleniumhq.org/selenium-ide/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              the Selenium IDE project page
+              Selenium IDE项目页面
             </a>
-            .
+            。
           </p>
         )}
       >
         <div>
-          <div>What would you like to do?</div>
+          <div>你想做什么？</div>
           <ul className="welcome-dialog__options">
             <li>
               <a onClick={this.startRecordingInNewProject}>
-                Record a new test in a new project
+                在新项目中录制新测试
               </a>
             </li>
             <li className="file-open">
               <OpenInput
                 onFileSelected={this.openProject}
-                labelMarkup={<div>Open an existing project</div>}
+                labelMarkup={<div>打开一个现有项目</div>}
               />
             </li>
             <li>
-              <a onClick={this.createNewProject}>Create a new project</a>
+              <a onClick={this.createNewProject}>创建一个新项目</a>
             </li>
             <li>
-              <a onClick={this.dismiss}>Close Selenium IDE</a>
+              <a onClick={this.dismiss}>关闭Selenium IDE</a>
             </li>
           </ul>
         </div>
