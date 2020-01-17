bb605b01b3819dc9ab340bb96ee2b94f75fd5f05
meter <772413635@qq.com>
2020-01-17 14:33:44 +0800

    ....

--- packages/code-export-robotframework/__test__/src/index.spec.js
+++ packages/code-export-robotframework/__test__/src/index.spec.js
@@ -45,6 +45,7 @@ describe('Code Export Python pytest', () => {
       test: project.tests[0],
       tests: project.tests,
     })
+    console.log(results.body)
     expect(results.body).toBeDefined()
     expect(results.body).toMatchSnapshot()
   })
@@ -99,7 +100,6 @@ describe('Code Export Python pytest', () => {
       suite: project.suites[0],
       tests: project.tests,
     })
-    console.log(results.body)
     expect(results.body).toBeDefined()
     expect(results.body).toMatchSnapshot()
   })
