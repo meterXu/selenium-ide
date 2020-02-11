0fd1dc593df7b04d78289572a711a1f713d385fc
许宏刚 <xuhonggang@hotmail.com>
2020-02-11 18:06:03 +0800

    ...

--- packages/selenium-ide/src/common/commandType.js
+++
@@ -1,7 +0,0 @@
-import i18n from '../neo/i18n/index'
-export default {
-  click: i18n.lang.commandType.click,
-  open: i18n.lang.commandType.open,
-  'set window size': i18n.lang.commandType.set_window_size,
-  type: i18n.lang.commandType.type,
-}
