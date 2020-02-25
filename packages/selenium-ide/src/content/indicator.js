fac197cd34e812d4d53704fd2f1e2e337443d07b
meter <772413635@qq.com>
2020-02-25 20:38:41 +0800

    ....

--- packages/selenium-ide/src/content/indicator.js
+++ packages/selenium-ide/src/content/indicator.js
@@ -7,7 +7,7 @@ var handleMessage = (function() {
   )
   var lastTimeoutHandle = 0
   function resetContent() {
-    content.innerText = 'JetRecord is recording...'
+    content.innerText = 'JetRecord 正在录制中...'
     content.style.color = '#E80600'
     circle.style.animation = 'fadeIn 1s infinite alternate'
     circle.style.visibility = 'visible'
@@ -15,7 +15,7 @@ var handleMessage = (function() {
   return function(event) {
     if (event.data && event.data.direction === 'from-recording-module') {
       clearTimeout(lastTimeoutHandle)
-      content.innerText = 'Recorded ' + event.data.command
+      content.innerText = '已录制 ' + event.data.command
       content.style.color = '#114990'
       circle.style.animation = 'none'
       circle.style.visibility = 'hidden'
