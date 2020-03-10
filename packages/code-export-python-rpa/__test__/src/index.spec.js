8e458a0ded15e6dce8545d5cccca69ff9e0887b6
meter <772413635@qq.com>
2020-03-10 14:27:35 +0800

    ....

--- packages/code-export-python-rpa/__test__/src/index.spec.js
+++ packages/code-export-python-rpa/__test__/src/index.spec.js
@@ -49,92 +49,92 @@ describe('Code Export Python pytest', () => {
     expect(results.body).toBeDefined()
     expect(results.body).toMatchSnapshot()
   })
-  it('should export a test with grid execution', async () => {
-    const project = readFile('single-test.side')
-    const results = await emitTest({
-      baseUrl: project.url,
-      test: project.tests[0],
-      tests: project.tests,
-      beforeEachOptions: {
-        browserName: 'Firefox',
-        gridUrl: 'http://localhost:4444/wd/hub',
-      },
-    })
-    expect(results.body).toBeDefined()
-    expect(results.body).toMatchSnapshot()
-  })
-  it('should export a suite', async () => {
-    const project = normalizeProject(readFile('single-suite.side'))
-    const results = await emitSuite({
-      baseUrl: project.url,
-      suite: project.suites[0],
-      tests: project.tests,
-    })
-    expect(results.body).toBeDefined()
-    expect(results.body).toMatchSnapshot()
-  })
-  it('should export a test with a reused test method', async () => {
-    const project = normalizeProject(readFile('test-case-reuse.side'))
-    const results = await emitTest({
-      baseUrl: project.url,
-      test: project.tests[1],
-      tests: project.tests,
-    })
-    // console.log(results.body)
-    expect(results.body).toBeDefined()
-    expect(results.body).toMatchSnapshot()
-  })
-  it('should export a suite with a reused test method', async () => {
-    const project = normalizeProject(readFile('test-case-reuse.side'))
-    const results = await emitSuite({
-      baseUrl: project.url,
-      suite: project.suites[0],
-      tests: project.tests,
-    })
-    expect(results.body).toBeDefined()
-    expect(results.body).toMatchSnapshot()
-  })
-  it('should export a suite that uses control flow commands', async () => {
-    const project = normalizeProject(readFile('control-flow-suite.side'))
-    const results = await emitSuite({
-      baseUrl: project.url,
-      suite: project.suites[0],
-      tests: project.tests,
-    })
-    // console.log(results.body)
-    expect(results.body).toBeDefined()
-    expect(results.body).toMatchSnapshot()
-  })
-  it('should export a test with commands that open a new window', async () => {
-    const project = normalizeProject(readFile('select-window.side'))
-    const results = await emitTest({
-      baseUrl: project.url,
-      test: project.tests[0],
-      tests: project.tests,
-    })
-    expect(results.body).toBeDefined()
-    expect(results.body).toMatchSnapshot()
-  })
-  it('should export a suite with commands that open a new window inside of a reused test method', async () => {
-    const project = normalizeProject(readFile('nested-select-window.side'))
-    const results = await emitSuite({
-      baseUrl: project.url,
-      suite: project.suites[0],
-      tests: project.tests,
-    })
-    expect(results.body).toBeDefined()
-    expect(results.body).toMatchSnapshot()
-  })
-  it('should export a suite with just one new window util method when there are multiple commands that open a new window', async () => {
-    const project = normalizeProject(readFile('nested-select-window-v2.side'))
-    const results = await emitSuite({
-      baseUrl: project.url,
-      suite: project.suites[0],
-      tests: project.tests,
-    })
-    expect(results.body).toBeDefined()
-    expect(results.body).toMatchSnapshot()
-  })
+  // it('should export a test with grid execution', async () => {
+  //   const project = readFile('single-test.side')
+  //   const results = await emitTest({
+  //     baseUrl: project.url,
+  //     test: project.tests[0],
+  //     tests: project.tests,
+  //     beforeEachOptions: {
+  //       browserName: 'Firefox',
+  //       gridUrl: 'http://localhost:4444/wd/hub',
+  //     },
+  //   })
+  //   expect(results.body).toBeDefined()
+  //   expect(results.body).toMatchSnapshot()
+  // })
+  // it('should export a suite', async () => {
+  //   const project = normalizeProject(readFile('single-suite.side'))
+  //   const results = await emitSuite({
+  //     baseUrl: project.url,
+  //     suite: project.suites[0],
+  //     tests: project.tests,
+  //   })
+  //   expect(results.body).toBeDefined()
+  //   expect(results.body).toMatchSnapshot()
+  // })
+  // it('should export a test with a reused test method', async () => {
+  //   const project = normalizeProject(readFile('test-case-reuse.side'))
+  //   const results = await emitTest({
+  //     baseUrl: project.url,
+  //     test: project.tests[1],
+  //     tests: project.tests,
+  //   })
+  //   // console.log(results.body)
+  //   expect(results.body).toBeDefined()
+  //   expect(results.body).toMatchSnapshot()
+  // })
+  // it('should export a suite with a reused test method', async () => {
+  //   const project = normalizeProject(readFile('test-case-reuse.side'))
+  //   const results = await emitSuite({
+  //     baseUrl: project.url,
+  //     suite: project.suites[0],
+  //     tests: project.tests,
+  //   })
+  //   expect(results.body).toBeDefined()
+  //   expect(results.body).toMatchSnapshot()
+  // })
+  // it('should export a suite that uses control flow commands', async () => {
+  //   const project = normalizeProject(readFile('control-flow-suite.side'))
+  //   const results = await emitSuite({
+  //     baseUrl: project.url,
+  //     suite: project.suites[0],
+  //     tests: project.tests,
+  //   })
+  //   // console.log(results.body)
+  //   expect(results.body).toBeDefined()
+  //   expect(results.body).toMatchSnapshot()
+  // })
+  // it('should export a test with commands that open a new window', async () => {
+  //   const project = normalizeProject(readFile('select-window.side'))
+  //   const results = await emitTest({
+  //     baseUrl: project.url,
+  //     test: project.tests[0],
+  //     tests: project.tests,
+  //   })
+  //   expect(results.body).toBeDefined()
+  //   expect(results.body).toMatchSnapshot()
+  // })
+  // it('should export a suite with commands that open a new window inside of a reused test method', async () => {
+  //   const project = normalizeProject(readFile('nested-select-window.side'))
+  //   const results = await emitSuite({
+  //     baseUrl: project.url,
+  //     suite: project.suites[0],
+  //     tests: project.tests,
+  //   })
+  //   expect(results.body).toBeDefined()
+  //   expect(results.body).toMatchSnapshot()
+  // })
+  // it('should export a suite with just one new window util method when there are multiple commands that open a new window', async () => {
+  //   const project = normalizeProject(readFile('nested-select-window-v2.side'))
+  //   const results = await emitSuite({
+  //     baseUrl: project.url,
+  //     suite: project.suites[0],
+  //     tests: project.tests,
+  //   })
+  //   expect(results.body).toBeDefined()
+  //   expect(results.body).toMatchSnapshot()
+  // })
 })
 
 function normalizeProject(project) {
