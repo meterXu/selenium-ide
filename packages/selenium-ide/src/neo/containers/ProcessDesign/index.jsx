fce4a344c9bf18af1460f77208fe810b0b606a0b
meter <772413635@qq.com>
2020-03-17 11:43:12 +0800

    ....

--- packages/selenium-ide/src/neo/containers/ProcessDesign/index.jsx
+++ packages/selenium-ide/src/neo/containers/ProcessDesign/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import ReactDom from 'react-dom'
+import Raphael from 'raphael'
+import './style.css'
 class ProcessDesign extends React.Component {
   constructor(props) {
     super(props)
   }
+  componentDidMount() {
+    let container = ReactDom.findDOMNode(this.refs.container)
+    let paper = new Raphael(container,container.clientWidth,container.clientHeight)
+    paper.circle(200, 200, 50)
+  }
+
   render() {
     return (
       <div className="graphContainer">
-        <p>流程设计</p>
+        {/* eslint-disable-next-line react/no-string-refs */}
+        <div className="container" ref="container" />
       </div>
     )
   }
