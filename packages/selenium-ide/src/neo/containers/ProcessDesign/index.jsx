d453403c26e1eb14ee6713e857098a1a11c8a13b
meter <772413635@qq.com>
2020-03-17 15:46:47 +0800

    ....

--- packages/selenium-ide/src/neo/containers/ProcessDesign/index.jsx
+++ packages/selenium-ide/src/neo/containers/ProcessDesign/index.jsx
@@ -8,22 +8,22 @@ class ProcessDesign extends React.Component {
     super(props)
   }
   componentDidMount() {
-    // let container = ReactDom.findDOMNode(this.refs.container)
-    // let paper = new Raphael(
-    //   container,
-    //   container.clientWidth,
-    //   container.clientHeight
-    // )
-    // paper.circle(200, 200, 50)
+    let container = ReactDom.findDOMNode(this.refs.graph)
+    let paper = new Raphael(
+      container,
+      container.clientWidth,
+      container.clientHeight
+    )
+    paper.circle(200, 200, 50)
   }
 
   render() {
     return (
       <div className="processDesign">
         {/* eslint-disable-next-line react/no-string-refs */}
-        <div className="design-container" ref="container">
+        <div className="design-container">
           <div className="design-bg" />
-          <div className="design-graph" />
+          <div className="design-graph" ref="graph"/>
         </div>
         <DockBar />
       </div>
