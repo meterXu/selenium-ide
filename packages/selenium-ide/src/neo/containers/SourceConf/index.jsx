746e7f58578f107e924a3ade7c68bd33ee9c425c
meter <772413635@qq.com>
2020-03-27 11:26:21 +0800

    ....

--- packages/selenium-ide/src/neo/containers/SourceConf/index.jsx
+++ packages/selenium-ide/src/neo/containers/SourceConf/index.jsx
@@ -31,31 +31,59 @@ export default class SourceConf extends React.Component {
         <div className="sourceConf-list">
           <ul>
             <li className="sourceConf-list-item">
-              <div className="sourceConf-type-icon sc-type-icon-oracle">图标</div>
-              <label className="sourceConf-type-title">标题</label>
+              <div className="sourceConf-type-icon sc-type-icon-oracle"></div>
+              <label className="sourceConf-type-title">oracle</label>
+            </li>
+            <li className="sourceConf-list-item">
+              <div className="sourceConf-type-icon sc-type-icon-mysql"></div>
+              <label className="sourceConf-type-title">mysql</label>
+            </li>
+            <li className="sourceConf-list-item">
+              <div className="sourceConf-type-icon sc-type-icon-sqlserver"></div>
+              <label className="sourceConf-type-title">sqlserver</label>
+            </li>
+            <li className="sourceConf-list-item">
+              <div className="sourceConf-type-icon sc-type-icon-excel"></div>
+              <label className="sourceConf-type-title">excel</label>
+            </li>
+            <li className="sourceConf-list-item">
+              <div className="sourceConf-type-icon sc-type-icon-api"></div>
+              <label className="sourceConf-type-title">api</label>
+            </li>
+            <li className="sourceConf-list-item">
+              <div className="sourceConf-type-icon sc-type-icon-api"></div>
+              <label className="sourceConf-type-title">api</label>
+            </li>
+            <li className="sourceConf-list-item">
+              <div className="sourceConf-type-icon sc-type-icon-api"></div>
+              <label className="sourceConf-type-title">api</label>
+            </li>
+            <li className="sourceConf-list-item">
+              <div className="sourceConf-type-icon sc-type-icon-api"></div>
+              <label className="sourceConf-type-title">api</label>
+            </li>
+            <li className="sourceConf-list-item">
+              <div className="sourceConf-type-icon sc-type-icon-api"></div>
+              <label className="sourceConf-type-title">api</label>
+            </li>
+            <li className="sourceConf-list-item">
+              <div className="sourceConf-type-icon sc-type-icon-api"></div>
+              <label className="sourceConf-type-title">api</label>
+            </li>
+            <li className="sourceConf-list-item">
+              <div className="sourceConf-type-icon sc-type-icon-api"></div>
+              <label className="sourceConf-type-title">api</label>
+            </li>
+            <li className="sourceConf-list-item">
+              <div className="sourceConf-type-icon sc-type-icon-api"></div>
+              <label className="sourceConf-type-title">api</label>
+            </li>
+            <li className="sourceConf-list-item">
+              <div className="sourceConf-type-icon sc-type-icon-api"></div>
+              <label className="sourceConf-type-title">api</label>
             </li>
             <li className="sourceConf-list-item sourceConf-list-item-add">
-              <div className="sourceConf-type-icon">图标</div>
-              <label className="sourceConf-type-title">添加</label>
-            </li>
-            <li className="sourceConf-list-item sourceConf-list-item-add">
-              <div className="sourceConf-type-icon">图标</div>
-              <label className="sourceConf-type-title">添加</label>
-            </li>
-            <li className="sourceConf-list-item sourceConf-list-item-add">
-              <div className="sourceConf-type-icon">图标</div>
-              <label className="sourceConf-type-title">添加</label>
-            </li>
-            <li className="sourceConf-list-item sourceConf-list-item-add">
-              <div className="sourceConf-type-icon">图标</div>
-              <label className="sourceConf-type-title">添加</label>
-            </li>
-            <li className="sourceConf-list-item sourceConf-list-item-add">
-              <div className="sourceConf-type-icon">图标</div>
-              <label className="sourceConf-type-title">添加</label>
-            </li>
-            <li className="sourceConf-list-item sourceConf-list-item-add">
-              <div className="sourceConf-type-icon">图标</div>
+              <div className="sourceConf-type-icon sc-type-icon-add"></div>
               <label className="sourceConf-type-title">添加</label>
             </li>
           </ul>
