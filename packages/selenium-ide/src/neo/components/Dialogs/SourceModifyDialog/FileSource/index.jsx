a878f67b33bc5506b2181615f72e619e20b9ff7f
meter <772413635@qq.com>
2020-03-30 17:32:33 +0800

    ....

--- packages/selenium-ide/src/neo/components/Dialogs/SourceModifyDialog/FileSource/index.jsx
+++ packages/selenium-ide/src/neo/components/Dialogs/SourceModifyDialog/FileSource/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './style.css'
 import FormInput from '../../../FormInput'
+import FormSelect from '../../../FormSelect'
 export default class FileSource extends React.Component {
   constructor(props) {
     super(props)
@@ -9,6 +10,7 @@ export default class FileSource extends React.Component {
     return (
       <>
         <FormInput label="名称" name="name" />
+        <FormSelect label="类型" name="type" />
       </>
     )
   }
