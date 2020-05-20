30a7aef5568f8e3d18b269d835053b0070d55b9f
meter <772413635@qq.com>
2020-05-20 15:12:03 +0800

    ...

---
+++ packages/selenium-ide/src/common/enum.js
@@ -0,0 +1,36 @@
+export default {
+    scIOType:{
+        "读取":0,
+        "写入":1
+    },
+    scType:{
+      "文件":0,
+      "数据库":1,
+      "接口":2
+    },
+    scFileType:{
+        "excel":0
+    },
+    scDbType:{
+        "oracle":0,
+        "sqlServer":1,
+        "mysql":2
+    },
+    scApiType:{
+      "post":"POST",
+      "get":"Get"
+    },
+    scTypeName:{
+        "excel":"excel",
+        "oracle":"oracle",
+        "sqlserver":"sqlserver",
+        "mysql":"mysql",
+        "api":"api"
+    },
+    btnType:{
+        '添加':0,
+        '修改':1,
+        '无':2
+    }
+
+}
