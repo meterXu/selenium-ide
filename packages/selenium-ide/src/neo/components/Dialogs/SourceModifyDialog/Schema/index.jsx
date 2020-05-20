b5b63b66078aca0d828145710d0b505c2e3e10c6
meter <772413635@qq.com>
2020-05-20 15:08:19 +0800

    ...

---
+++ packages/selenium-ide/src/neo/components/Dialogs/SourceModifyDialog/Schema/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './style.css'
+import PropTypes from 'prop-types'
+export  default  class Schema extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    static propTypes={
+        data:PropTypes.array
+    }
+    render() {
+        return (
+            <ul className={'schema-container'}>
+                {
+                    this.props.data.map((c,i)=>{
+                        return (
+                            <li key={i}>{c}</li>
+                        )
+                    })
+                }
+
+            </ul>
+        )
+    }
+}
